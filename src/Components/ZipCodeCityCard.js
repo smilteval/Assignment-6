513a372252e7fa8bcdd20ed3cc6546c2883fe70b
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-12 15:52:02 -0500

    removed city card component and created a static card inside the city list

--- src/Components/ZipCodeCityCard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ZipCodeCityCard() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- src/Components/ZipCodeCityList.js
+++ src/Components/ZipCodeCityList.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import ZipCodeCityCard from './ZipCodeCityCard'
 
 export default function ZipCodeCityList() {
     return (
-        <div>
-            <ZipCodeCityCard/>
+        //static city card 
+        <div id="city-card">
+            <h2>New York, NY</h2>
+            <div id ="city-info">
+                <p id="state">New York</p>
+                <p id="location">(00,00)</p>
+                <p id="population"> 1312159653</p>
+                <p id="wages"> 564561</p>
+            </div>
         </div>
     )
 }
