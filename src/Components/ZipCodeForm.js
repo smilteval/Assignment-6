0a9940a59c4a3d4ccd75cd668a7b227e843fc7b0
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-13 00:19:11 -0500

    changed functional component to a class

--- src/Components/ZipCodeForm.js
+++ src/Components/ZipCodeForm.js
@@ -1,22 +1,29 @@
-import React, {useState} from 'react'
+import React, { Component } from 'react'
 import ZipCodeCityCard from './ZipCodeCityCard'
 
-export default function ZipCodeForm() {
+export class ZipCodeFormClass extends Component {
 
-    let [zipCode,setZipCode] = useState(null);
-    let [cityData,setCityData] = useState([]);
-    let [noResultText, setNoResultText] = useState("No Result");
+    constructor(){
+        super();
+        this.state = {
+            zipCode: null,
+            cityData: [],
+            noResultText: " ",
+        };
+    }
 
     //every time a user enters a zip code, update it
-    let handleChange = event => {
-        setZipCode(event.target.value);
+    handleChange = event => {
+        console.log("handleChange")
+        this.setState({zipCode: event.target.value});
     }
 
     //get data from an api
-    const getData = async () => {
+    getData = async () => {
+        console.log("inside getData")
         try{
             //get a response from an api
-            let response = await fetch("https://ctp-zip-api.herokuapp.com/zip/" + zipCode);
+            let response = await fetch("https://ctp-zip-api.herokuapp.com/zip/" + this.state.zipCode);
             
             //if there was an error, print it
             if(!response.ok){
@@ -26,50 +33,54 @@ export default function ZipCodeForm() {
             //turn the response into a data object
             let data = await response.json();
 
-            //set the city data to the data received from an api
-            setCityData(data);
-
-            //set the <p> text to an empty string
-            setNoResultText(" ");
+            this.setState({
+                cityData: data, //set the city data to the data received from an api
+                noResultText: " " //set the text of <p> tag
+            });
         }
+
         catch(error){
             console.log(error);
-            setCityData([]);
-            setNoResultText("No Result");
+            this.setState({
+                cityData: [],
+                noResultText: "No Result"
+            });
         }
-    }
+    }    
 
-    getData();
-
-    return (
-        <div>
-            <header>
+    render() {
+        return (
+            <div>
+                <header>
                 <h1>Zip Code Search</h1>
-            </header>
-            <div id="search-section">
-                <label>Zip Code:</label>
-                <input 
-                    id="zip-code-input"  
-                    type="search" 
-                    placeholder="Try 10016"
-                    onChange={handleChange}
-                />
+                </header>
+                <div id="search-section">
+                    <label>Zip Code:</label>
+                    <input 
+                        id="zip-code-input"  
+                        type="search" 
+                        placeholder="Try 10016"
+                        onChange={this.handleChange}
+                    />
+                    <button onClick={()=>{this.getData()}}>Search</button>
+                </div>
+                <p id ="zip-not-found">{this.state.noResultText}</p>
+                {/* for every city found, print its details */}
+                {this.state.cityData.map(city=>{
+                    // passing all the data values to another component
+                    return <ZipCodeCityCard
+                        LocationText = {city.LocationText}
+                        State = {city.State}
+                        Lat = {city.Lat}
+                        Long = {city.Long}
+                        EstimatedPopulation = {city.EstimatedPopulation}
+                        TotalWages = {city.TotalWages}
+                    />
+                })}
+            
             </div>
-            <p id ="zip-not-found">{noResultText}</p>
-
-            {/* for every city found, print its details */}
-            {cityData.map(city=>{
-                // passing all the data values to another component
-                return <ZipCodeCityCard
-                    LocationText = {city.LocationText}
-                    State = {city.State}
-                    Lat = {city.Lat}
-                    Long = {city.Long}
-                    EstimatedPopulation = {city.EstimatedPopulation}
-                    TotalWages = {city.TotalWages}
-                />
-            })}
-
-        </div>
-    )
+        )
+    }
 }
+
+export default ZipCodeFormClass
